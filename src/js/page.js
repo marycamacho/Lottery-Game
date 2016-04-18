8889a9a7cae8496eff53eca8a2832a283c9fa3e7
Mary Camacho <maryfcamacho@gmail.com>
2016-04-17 19:45:10 -0600

    all updates for project (mary was stupid and didn't do commits throughout the work-- I would have strongly critiqued anyone on my team for doing such a large commit.

--- src/js/page.js
+++ src/js/page.js
@@ -2,72 +2,147 @@
  * Created by mary on 04/16/2016.
  */
 
-/*$(function(){
+$(function(){
+    var pickedNumbers = [];
+    var randomNumbers = [];
+    var balanceAmt = 10;
+    var recordScore =10;
 
-    $(".goal-display").each(function() {
+    //display beginning balance and record winnings and handlePlayButton
+    $('.balance').text(balanceAmt);
+    $('.currentWinnings').text(0);
+    $('.recordScore').text(balanceAmt);
+    handlePlayButton();
 
+    //everything that happens when the user clicks on one of the numbers in the boxes
+    $(".num-select").click(function () {
         var this$ = $(this);
-        var numberSquares = this$.data().showSquares;
-        var controlName = this$.data().name;
-        var goalType = this$.data().goalType;
+        var child$ = this$.children();
+        var pickedNum = child$.data().num;
 
-        this$.append('<h3>' + this$.data().title + '</h3>');
-
-        // commented code here is to add display based on over/under
-        //if(goalType == "over") {
-            for (var i = 0; i < numberSquares; i++) {
-                this$.append('<div data-cell="' + controlName + (i + 1) + '" class="pull-left square"></div>');
+        // boxes change colors until 4 numbers are selected at which point error is displayed
+        if (child$.hasClass('num-box-green')) {
+            if($('.num-box-yellow').length < 4) {
+                child$.addClass('num-box-yellow').removeClass('num-box-green');
+                pickedNumbers.push(pickedNum);
+            } else {
+                alert("You can only select 4 numbers. If you want a different number, unselect one of your numbers and pick a different one.");
             }
-        /!*} else {
-            this$.append('<div data-cell="' + controlName + (i + 1) + '" class="pull-left square"></div>');
-        }*!/
-
+        } else {
+            child$.addClass('num-box-green').removeClass('num-box-yellow');
+            var removeIndex = pickedNumbers.indexOf(pickedNum);
+            if (removeIndex > -1) {
+                pickedNumbers.splice(removeIndex, 1);
+            }
+        }
+        paintPickedNums();
+        handlePlayButton();
+        console.log(pickedNumbers);
+        //saveData(data);
     });
 
-    var data = getData();
-    var allSquares$ = $(".square");
-
-    function paintSquares(){
-        allSquares$.each(function () {
-            var this$ = $(this);
-            var cellID = this$.data().cell;
-            if (data[cellID]) {
-                if (data[cellID] == 1) {
-                    this$.addClass('single');
-                } else {
-                    this$.addClass('double');
-                }
-            }
+    //paint the picked numbers on the first row of white balls
+    function paintPickedNums () {
+        $('.numPicks .ball').text('');
+        $.each(pickedNumbers, function(index, value) {
+            $('.numPicks .pickedIndex'+index).text(value);
         });
     }
-
-    paintSquares();
-
-    $('#resetBtn').click(function(){
-        allSquares$.removeClass('single').removeClass('double');
-        data=resetData();
-        paintSquares();
-    });
-
-    allSquares$.click(function () {
-        var this$ = $(this);
-        var cellID = this$.data().cell;
-        var goalType = this$.data().goalType;
-        var controlName = this$.data().name;
-
-        if (this$.hasClass('single')){
-            this$.addClass('double').removeClass('single');
-            data[cellID] = 2;
-        } else if (this$.hasClass('double')){
-            this$.removeClass('double');
-            data[cellID] = 0;
-
+    // put into a function that can be called from many places
+    function handlePlayButton () {
+        var allPlay$ = $(".play");
+        if (balanceAmt >= 2 && pickedNumbers.length == 4) {
+            allPlay$.addClass('playButton').removeClass('playButtonGrey');
         } else {
-            this$.addClass('single');
-            data[cellID] = 1;
+            allPlay$.addClass('playButtonGrey').removeClass('playButton');
+        }
+    }
+    
+    // everything that happens when user clicks play now button
+    $(".play").click(function () {
+        if (! $(this).hasClass('playButton')) {
+            return
+        }
+        // clear old number from house balls and random array
+        $('.numRandoms .ball').text('');
+
+        // deduct $2 from balance for playing game
+        balanceAmt = balanceAmt - 2;
+        $('.balance').text(balanceAmt);
+
+        // add spin class to balls for 3 seconds, then remove
+        var spinBall$ = $(".numRandoms .ball");
+        spinBall$.addClass('spin');
+        setInterval(function(){
+            spinBall$.removeClass("spin");
+
+        }, 3000);
+
+        // randomly calculate a number min and max inclusive
+        function randomGenerator (min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
         }
 
-        saveData(data);
-        });
-});*/
+    //after a 3+ second delay, for each .numRandoms and .ball in the dom, run the Random# function and paint the result in the div and save it to the array.
+
+        //painting numbers into balls for random selected numbers
+        function paintRandomNums () {
+            $('.numRandoms .ball').text('');
+            $.each(randomNumbers, function(index, value) {
+                $('.numRandoms .pickedIndex'+index).text(value);
+            });
+        }
+
+        var pickRandom$ = $(".numRandoms .ball");
+        randomNumbers = [];
+
+        setTimeout(function(){
+
+            pickRandom$.each(function (index, el) {
+                var pickedRandomNumber = randomGenerator(1,10);
+                randomNumbers.push(pickedRandomNumber);
+            });
+            paintRandomNums();
+
+            var winnings = calculateWinnings();
+            $('.currentWinnings').text(winnings);
+            console.log(winnings);
+
+            // calculate and display new balance
+
+            if(winnings > 0) {
+                balanceAmt = balanceAmt + winnings;
+                $('.balance').text(balanceAmt);
+            }
+
+            if (balanceAmt > recordScore) {
+                $('.recordScore').text(balanceAmt);
+            }
+
+        }, 2500);
+
+        // compare arrays of numbers (picked & random):  for each picked number, analyze if it is in the random array. For every one that is a yes, calculate and sum winnings.
+
+        function calculateWinnings (){
+            var countOfMatches = 0;
+            for (var i = 0; i < pickedNumbers.length; i++) {
+                if(randomNumbers.indexOf(pickedNumbers[i]) > -1) {
+                    countOfMatches++;
+                }
+            }
+            console.log(countOfMatches);
+
+            //calculate winnings for ticket - if 0 wins, return 0 winnings
+            //decided to give more
+            if(countOfMatches == 0){
+                return 0;
+            } else {
+                return Math.pow(2,countOfMatches);
+            }
+        }
+    });
+});
+
+
+
 
